7c475bb6fc45006a310d2b93caa99f515082f0c2
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-23 20:56:05 -0400

    Intialize React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>BetterBeer</h1>
+        <h2>Get the best prices from the locals you trust!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
