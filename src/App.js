9393ac9ee62216d7391a5c96cadecd1a42f5ff72
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-28 20:56:57 -0400

    Build beer submit form, WIP: Actually make it post

--- src/App.js
+++ src/App.js
@@ -20,15 +20,42 @@ class App extends Component {
         console.log(filteredFeed);
       })
   }
+  //For the actual beer submission form, move to new component
+  handleNewBeerSubmit(e) {
+    //e.preventDefault();
+    console.log(e);
+  }
   render() {
     return (
       <div>
         <h1>BetterBeer</h1>
         <h2>Get the best prices from the locals you trust!</h2>
+        <h3>Beer Feed:</h3>
         <button onClick={(e) => this.handleGetFeed()}>Feed!</button>
         <form action="/" onSubmit={(e) => this.handleFilteredFeed(e, document.getElementById('beer').value)}>
+        <h3>Search for a beer:</h3>
           <input id="beer" type="text" name="beer" placeholder="ex. 'Wakulla', 'Big Nose'"/>
-          <button type='submit'>Filtered</button>
+          <button type="submit">Filtered</button>
+        </form>
+        <form action="/submissions" method="get" onSubmit={(e) => this.handleNewBeerSubmit(e)}>
+          <h3>Spotted a better deal? Submit it here:</h3>
+          <input type="text" name="name" placeholder="Name of beer" required/>
+          <br/>
+          <label>$<input type="float" name="price" placeholder="4.00" required/></label>
+          <br/>
+          <select name="purchaseType" required>
+          <option value="" selected disabled hidden>Choose type of purchase...</option>
+            <option value="bottle">Single Bottle</option>
+            <option value="draft">Draft Pour</option>
+            <option value="can">Single Can</option>
+            <option value="6pk">6-Pack</option>
+            <option value="12pk">12-Pack</option>
+            <option value="24pk">24-Pack</option>
+          </select>
+          <br/>
+          <input type="float" name="lattitude" value="12.3456" hidden/>
+          <input type="float" name="longitude" value="-65.4321" hidden/>
+          <button type="submit">Submit Find</button>
         </form>
         <BeerEntry />
       </div>
