21b76ebf437fea7000b3efd1222d2d3c441b50c4
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-05 18:29:29 -0400

    Create Navigation component and render in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
 import './App.css';
 //components
+import Navigation from './components/Navigation';
 import BeerFeed from './components/BeerFeed';
 import BeerSearch from './components/BeerSearch'
 import BeerSubmission from './components/BeerSubmission'
@@ -13,7 +14,7 @@ class App extends Component {
       <div>
         <h1>BetterBeer</h1>
         <h2>Get the best prices from the locals you trust!</h2>
-        
+        <Navigation />
         <Route exact path='/' component={BeerFeed} />
         <Route exact path='/search' component={BeerSearch} />
         <Route exact path='/submission' component={BeerSubmission} />
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = props => (
+    <nav>
+        <ul>
+            <li><button>Home</button></li>
+            <li><button>Search</button></li>
+            <li><button>Submit</button></li>
+        </ul>
+    </nav>
+);
+
+export default Navigation;
