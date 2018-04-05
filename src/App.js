534843a0477d39047f58c4a531836fc2e09568cf
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-05 18:23:20 -0400

    Install react-router-dom, get Routes active for feed, search, and sumission components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom';
 import './App.css';
+//components
 import BeerFeed from './components/BeerFeed';
 import BeerSearch from './components/BeerSearch'
 import BeerSubmission from './components/BeerSubmission'
@@ -11,12 +13,14 @@ class App extends Component {
       <div>
         <h1>BetterBeer</h1>
         <h2>Get the best prices from the locals you trust!</h2>
-        <BeerSearch />
-        <BeerFeed />
-        <BeerSubmission />
+        
+        <Route exact path='/' component={BeerFeed} />
+        <Route exact path='/search' component={BeerSearch} />
+        <Route exact path='/submission' component={BeerSubmission} />
+        
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
