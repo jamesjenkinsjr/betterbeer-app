22caaf7b2f746dff727945dec4f0e79ec12685c3
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-05 19:10:56 -0400

    Move title-case into utilities file

--- src/components/BeerEntry.js
+++ src/components/BeerEntry.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import titleCase from 'title-case'; //util to make everything title case ex. big nose > Big Nose
-import { prettyDate } from '../utilities';
+import React from 'react'; //util to make everything title case ex. big nose > Big Nose
+import { prettyDate, uppercase } from '../utilities';
 
 const BeerEntry = (props) => {
 
         return (
             <div key={props._id}>
-              <h3>{titleCase(props.name)}</h3>
+              <h3>{uppercase(props.name)}</h3>
               <ul>
                 <li>Price: {props.price}</li>
                 <li>Spotted on: {prettyDate(props.createTimestamp)}</li>
--- src/utilities.js
+++ src/utilities.js
@@ -1,6 +1,12 @@
+import titleCase from 'title-case';
+
 export const prettyDate = (ts) => {
     const d = new Date(ts)
     const date = d.toLocaleString();
     const tz = d.toLocaleTimeString('en-us',{timeZoneName:'short'}).split(' ')[2];
     return date + ' ' + tz;
+}
+
+export const uppercase = (string) => {
+    return titleCase(string);
 }
