65466127dbd71a7a910c950bda3ef9fc2e91bc6e
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-09 19:00:51 -0400

    Add bulma css to index.css, begin applying styles to BeerSubmission

--- src/components/BeerSubmission.js
+++ src/components/BeerSubmission.js
@@ -8,11 +8,11 @@ class BeerSubmission extends Component {
     super();
     this.state = {
       entry: {
-        name: '',
-        price: '',
-        purchaseType: '',
-        location: '',
-        placeID: ''
+        name: "",
+        price: "",
+        purchaseType: "",
+        location: "",
+        placeID: ""
       },
       search: "",
       results: []
@@ -47,13 +47,12 @@ class BeerSubmission extends Component {
     this.setState({
       entry: {
         ...this.state.entry,
-      location: result.description,
-      placeID: result.place_id
+        location: result.description,
+        placeID: result.place_id
       },
-      search: '',
+      search: "",
       results: []
-    })
-
+    });
   }
   handlePurchaseType(e) {
     this.setState({
@@ -61,7 +60,7 @@ class BeerSubmission extends Component {
         ...this.state.entry,
         purchaseType: e.target.value
       }
-    })
+    });
   }
 
   render() {
@@ -69,82 +68,125 @@ class BeerSubmission extends Component {
       <div>
         <form onSubmit={e => this.handleNewBeerSubmit(e)}>
           <h3>Spotted a better deal? Submit it here:</h3>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name of beer"
-            value={this.state.entry.name}
-            onChange={e => {
-              this.setState({
-                entry: {
-                  ...this.state.entry,
-                  name: e.target.value
-                }
-              });
-            }}
-            required
-          />
-          <br />
-          <label>
-            $
-            <input
-              type="number"
-              step="0.01"
-              name="price"
-              placeholder="4.00"
-              value={this.state.entry.price}
-              onChange={e => {
-                this.setState({
-                  entry: {
-                    ...this.state.entry,
-                    price: +e.target.value
-                  }
-                });
-              }}
-              required
-            />
-          </label>
-          <br />
-          <input type="disabled" value={this.state.entry.location} placeholder='Searching...'/>
-          <br/>
-          <label>
-            Find Location
-            <input
-              placeholder="Ex. Cigar City, Gator Beverage, etc."
-              onChange={e => this.handleGoogleSearch(e)}
-            />
-          </label>
-          {this.state.search
-            ?
-                this.state.results.map(result => {
-                  console.log(result);
-                return( 
-                  <ul key={result.id}>
-                    <li><button onClick={() => this.handleSetLocation(result)}>{result.description}</button></li>
-                  </ul>
-                
+          <div class="field">
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                name="name"
+                placeholder="Name of beer"
+                value={this.state.entry.name}
+                onChange={e => {
+                  this.setState({
+                    entry: {
+                      ...this.state.entry,
+                      name: e.target.value
+                    }
+                  });
+                }}
+                required
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">
+              Price
+              <div class="control">
+                <input
+                  class="input"
+                  type="number"
+                  step="0.01"
+                  name="price"
+                  placeholder="$4.00"
+                  value={this.state.entry.price}
+                  onChange={e => {
+                    this.setState({
+                      entry: {
+                        ...this.state.entry,
+                        price: +e.target.value
+                      }
+                    });
+                  }}
+                  required
+                />
+              </div>
+            </label>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <input
+                class="input"
+                type="disabled"
+                value={this.state.entry.location}
+                placeholder="Searching..."
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">
+              Find Location
+              <div class="control">
+                <input
+                  class="input"
+                  placeholder="Ex. Cigar City, Gator Beverage, etc."
+                  onChange={e => this.handleGoogleSearch(e)}
+                />
+              </div>
+            </label>
+          </div>
+          {this.state.search ? (
+            <div class="buttons">
+              {this.state.results.map(result => {
+                return (
+                  <button
+                    key={result.id}
+                    class="button is-secondary"
+                    onClick={() => this.handleSetLocation(result)}
+                  >
+                    {result.description}
+                  </button>
                 );
-                })
-              
-            :   "Locations will appear here"}
-          <br />
-          <select
-            name="purchaseType"
-            onChange={(e) => this.handlePurchaseType(e)}
-            required
-          >
-            <option value="" selected disabled hidden>
-              Choose type of purchase...
-            </option>
-            <option value="bottle">Single Bottle</option>
-            <option value="draft">Draft Pour</option>
-            <option value="can">Single Can</option>
-            <option value="6pk">6-Pack</option>
-            <option value="12pk">12-Pack</option>
-            <option value="24pk">24-Pack</option>
-          </select>
-          <br />
-          <button type="submit">Submit Find</button>
+              })}
+            </div>
+          ) : (
+            <p class="help">Location results will appear here</p>
+          )}
+
+          <div class="field">
+            <label class="label">
+              Purchase Type?
+              <div class="control">
+                <div class="select">
+                  <select
+                    name="purchaseType"
+                    onChange={e => this.handlePurchaseType(e)}
+                    required
+                  >
+                    <option value="" selected disabled hidden>
+                      Choose type of purchase...
+                    </option>
+                    <option value="bottle">Single Bottle</option>
+                    <option value="draft">Draft Pour</option>
+                    <option value="can">Single Can</option>
+                    <option value="6pk">6-Pack</option>
+                    <option value="12pk">12-Pack</option>
+                    <option value="24pk">24-Pack</option>
+                  </select>
+                </div>
+              </div>
+            </label>
+          </div>
+
+          <div class="field is-grouped">
+            <div class="control">
+              <button class="button is-primary" type="submit">
+                Submit Find
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     );
