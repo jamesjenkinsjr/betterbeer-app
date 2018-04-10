ee3574394d58357b958f740ee14ee2dd7797d348
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-09 21:06:38 -0400

    Style Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = props => (
-    <nav>
-        <ul>
-            <li><button><Link to="/">Home</Link></button></li>
-            <li><button><Link to="/search">Search</Link></button></li>
-            <li><button><Link to="/submission">Submit</Link></button></li>
-        </ul>
+    <nav class='navbar is-transparent is-primary' role='navigation' >
+        <div class='navbar-menu'>
+            <li class='navbar-item'><button class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/">Home</Link></button></li>
+            <li class='navbar-item'><button class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/search">Search</Link></button></li>
+            <li class='navbar-item'><button class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/submission">Submit</Link></button></li>
+        </div>
     </nav>
 );
 
