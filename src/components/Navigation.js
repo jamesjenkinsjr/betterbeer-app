32f4a4a8760451a6192c484403c2c37bf3bb93bc
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-10 20:38:38 -0400

    Hamburger in Navigation toggles active on click

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = props => (
-    <nav class='navbar is-primary' role='navigation' >
-        
-        <div class="navbar-burger burger" data-target="menu">
-      <span></span>
-      <span></span>
-      <span></span>
+  <nav className="navbar is-primary" role="navigation">
+    <div className="navbar-burger burger" data-target="menu" onClick={() => {document.getElementById('menu').classList.toggle('is-active')}}>
+      <span />
+      <span />
+      <span />
     </div>
-   
-        <div id="menu" className="navbar-menu is-active">
-        <div className="navbar-start">
+
+    <div id="menu" className="navbar-menu">
+      <div className="navbar-start">
         <div className="navbar-item">
-        <div className="field is-grouped">
-        <p className="control">
-            <a class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/">Feed</Link></a>
-            <a class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/search">Search</Link></a>
-            <a class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/submission">Submit</Link></a>
+          <div className="field is-grouped">
+            <p className="control">
+              <a className="button is-large is-primary">
+                <Link style={{ textDecoration: "none", color: "white" }} to="/">
+                  Feed
+                </Link>
+              </a>
+              <a className="button is-large is-primary">
+                <Link
+                  style={{ textDecoration: "none", color: "white" }}
+                  to="/search"
+                >
+                  Search
+                </Link>
+              </a>
+              <a className="button is-large is-primary">
+                <Link
+                  style={{ textDecoration: "none", color: "white" }}
+                  to="/submission"
+                >
+                  Submit
+                </Link>
+              </a>
             </p>
-            </div>
-            </div>
-            </div>
+          </div>
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 );
 
 export default Navigation;
