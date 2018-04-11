9e6fa6422063623d20ee6384347409d995a123b2
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-10 20:12:59 -0400

    WIP Make hamburger menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = props => (
-    <nav class='navbar is-transparent is-primary' role='navigation' >
-        <div class='navbar-menu container'>
-            <li class='navbar-item'><button class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/">Feed</Link></button></li>
-            <li class='navbar-item'><button class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/search">Search</Link></button></li>
-            <li class='navbar-item'><button class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/submission">Submit</Link></button></li>
+    <nav class='navbar is-primary' role='navigation' >
+        
+        <div class="navbar-burger burger" data-target="menu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+   
+        <div id="menu" className="navbar-menu is-active">
+        <div className="navbar-start">
+        <div className="navbar-item">
+        <div className="field is-grouped">
+        <p className="control">
+            <a class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/">Feed</Link></a>
+            <a class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/search">Search</Link></a>
+            <a class='button is-large is-primary'><Link style={{ textDecoration: 'none', color: 'white' }} to="/submission">Submit</Link></a>
+            </p>
+            </div>
+            </div>
+            </div>
         </div>
     </nav>
 );
